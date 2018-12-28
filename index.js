190c2d1ff557c8d5ac8b7f9febcef7f6045e63fe
William Ulman <will.ulman@gmail.com>
2018-12-28 19:31:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  const shoutedString = string.toUpperCase();
+  console.log(shoutedString);
+}
+
+function logWhisper(string) {
+  const whisperedString = string.toLowerCase();
+  console.log(whisperedString);
+}
